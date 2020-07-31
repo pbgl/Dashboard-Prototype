ea99b31f3efb72ab19edfd76253bf4cf1723b5a1
Anza Ghaffar <anza.ghaffar@hotmail.com>
2020-08-01 00:01:38 +0300

    fixing the import commands

--- mutants_dashboard.py
+++ mutants_dashboard.py
@@ -43,10 +43,10 @@ tab_selected_style = {
 app = dash.Dash(__name__)
 
 # Importing data
-SnpSiftData=pd.read_csv(r'data/snpsiftdata.tab.txt',delimiter='\t',encoding='UTF-8')
-chromosome_name=pd.read_csv(r'data/chromosome_name_mapping.tab.txt',delimiter='\t',encoding='UTF-8')
+SnpSiftData=pd.read_csv(r'data/snpsiftdata.tab',delimiter='\t',encoding='UTF-8')
+chromosome_name=pd.read_csv(r'data/chromosome_name_mapping.tab',delimiter='\t',encoding='UTF-8')
 Genotype_Data=pd.read_csv(r'data/genotype_data.tab',delimiter='\t',encoding='UTF-8')
-passport=pd.read_csv(r'data/passport.tab.txt',delimiter='\t',encoding='UTF-8')
+passport=pd.read_csv(r'data/passport.tab',delimiter='\t',encoding='UTF-8')
 
 passport.replace(np.NaN,'NA',inplace=True)
 
